cfcc81256593b0e0da990d490fce4f749ae52336
kkrusemark <kkrusemark@unomaha.edu>
2025-09-14 19:20:11 +0000

    finished ApproxPi

--- ApproxPi.py
+++ ApproxPi.py
@@ -1,7 +1,7 @@
 #ApproxPi.py
-#Name:
-#Date:
-#Assignment:
+#Name: Kylie Krusemark
+#Date: 9/14/25
+#Assignment: Lab 3
 import math
 import time
 
@@ -12,14 +12,31 @@ def main():
   realPi = math.pi
 
   #ask user for decimal percision (up to 10)
+  
+  decimal = input("How many decimal places of Pi would you like? (up to 10): ")
+  decimal = int(decimal)
 
   start = time.time()
+
   #calculate pi using the approximation technique
+  
+  approxPi = 4/1
+  sign = -1
+  denominator = 3
+
   #Loop until the level of percision is reached
+  
+  while round(approxPi, decimal) != round(realPi, decimal): 
+    #print(approxPi)
+    approxPi = approxPi + (sign * 4 / denominator) 
+
+    sign = sign * -1
+    denominator =  denominator + 2
 
   end = time.time()
 
   elapsedTime = end - start
+  print("Pi:", round(approxPi, decimal))
   print(elapsedTime)
 
 if __name__ == '__main__':
